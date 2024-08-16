6e6e11febff78f15dff2021ba438c317fca96c57
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-08-16 17:55:43 +0900

    [♻️refactor]: 불필요한 코드 정리

--- pages/login/google-login.js
+++ pages/login/google-login.js
@@ -3,5 +3,5 @@ import { URL } from "/assets/js/constants";
 // Google 로그인 버튼 처리
 export async function handleGoogleLoginRedirect() {
   window.location.href = `${URL}/auth/google`; // 서버로 로그인 요청 보내기
-  sessionStorage.setItem("isSocialLogIn", true);
+  sessionStorage.setItem("isSocialLogIn", true); // 세션스토리지에 소셜로그인 여부 체크
 }
